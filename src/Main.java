9bc504a4a5fcb806aa5f0eb3567197090acd1c3f
BelukhinArtem <119603015+BelukhinArtem@users.noreply.github.com>
2022-12-02 13:15:38 +0300

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        int[] array = {53, 78, 13, 9, 5, 17, 28, 719, 214};
+        for (int i = 0; i < array.length; i++) {
+//          array[i] = (i+1)*2;
+//             System.out.println("Чётные значения = " + array[i]);
+
+            if (array[i] % 2 == 0) {
+                System.out.println("Number: " + array[i]);
+            }
+        }
+    }
+}
